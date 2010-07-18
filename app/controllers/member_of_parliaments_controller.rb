7b7c854f96710d9b2303225975969c9f2758abff
Akshay Rawat <akshay@activesphere.com>
2010-07-18 13:13:53 +0530

    a useful readme

--- app/controllers/member_of_parliaments_controller.rb
+++ app/controllers/member_of_parliaments_controller.rb
@@ -3,16 +3,16 @@ class MemberOfParliamentsController < ApplicationController
   def index
     mps = MemberOfParliament.all
     respond_to do |format|
-      format.xml {render :xml => mps.to_xml}
-      format.json {render :json => mps.to_json}
+      format.xml {render :xml => mps.to_xml(:only => [:id, :name])}
+      format.json {render :json => mps.to_json(:only => [:id, :name])}
     end
   end
 
   def show
     mp = MemberOfParliament.get(params[:id])
     respond_to do |format|
-      format.xml {render :xml => mp.to_xml}
-      format.json {render :json => mp.to_json}
+      format.xml {render :xml => mp.to_xml(:methods=> [:party, :constituency])}
+      format.json {render :json => mp.to_json(:methods=> [:party, :constituency])}
     end
   end
 
--- spec/controllers/member_of_parliaments_controller_spec.rb
+++ spec/controllers/member_of_parliaments_controller_spec.rb
@@ -5,35 +5,38 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe MemberOfParliamentsController do
   
   context "index" do
-    
-    before(:each) do
-      Factory.create(:member_of_parliament, :name => "Foo")
-      Factory.create(:member_of_parliament, :name => "Bar")      
-    end
-       
     it "should return all Member Of Parliament in json format" do
+      mp_one = Factory.create(:member_of_parliament, :name => "Foo")
+      mp_two = Factory.create(:member_of_parliament, :name => "Bar")      
+
       get :index, :format => :json
       
       mps = ActiveSupport::JSON.decode(response.body)
       mps.should have(2).things
-      mps.first["name"].should == "Foo"
-      mps.last["name"].should == "Bar"
+      mps.first["id"].should == mp_one.id
+      mps.first["name"].should == mp_one.name
+      mps.last["id"].should == mp_two.id
+      mps.last["name"].should == mp_two.name
     end
     
     it "should return all Member Of Parliament in xml format" do
+      mp_one = Factory.create(:member_of_parliament, :name => "Foo")
+      mp_two = Factory.create(:member_of_parliament, :name => "Bar")      
+      
       get :index, :format => :xml
       
       mps = Hash.from_xml(response.body)["member_of_parliaments"]
       
       mps.should have(2).things
-      mps.first["name"].should == "Foo"
-      mps.last["name"].should == "Bar"
+      mps.first["id"].should == mp_one.id
+      mps.first["name"].should == mp_one.name
+      mps.last["id"].should == mp_two.id
+      mps.last["name"].should == mp_two.name
     end
     
   end
   
   context "show" do
-    
     it "should return the specified Member Of Parliament as json" do
       mp = Factory.create(:member_of_parliament, :name => "Foo")
       get :show, :id => mp.id, :format => :json
