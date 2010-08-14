a590c7685470a09be723c27efcfe4c1da9085169
Akshay Rawat <akshay@activesphere.com>
2010-08-14 12:37:17 +0530

    made mymin search case insensitive

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,7 @@ class SearchController < ApplicationController
   }
 
   def index
-    results = params[:type].camelize.constantize.all(:name.like => "%#{params[:name]}%", :limit => 20)
+    results = params[:type].camelize.constantize.all(:conditions => ["upper(name) LIKE ?", "%#{params[:name].upcase}%"], :limit => 20)
     respond_to do |format|
       format.json {render :json => results.to_json}
       format.xml {render :xml => results.to_xml}
