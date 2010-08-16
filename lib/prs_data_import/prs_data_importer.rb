09d9a2641756180d7540e57dbbfd7c7fac55c633
Akshay Rawat <akshay@activesphere.com>
2010-08-16 14:18:04 +0530

    added deceased members

--- lib/prs_data_import/prs_data_importer.rb
+++ lib/prs_data_import/prs_data_importer.rb
@@ -22,28 +22,21 @@ class PrsDataImporter
   private 
   
   def find_constituency data
-    constituency = CONSTITUENCY_ALIASES[data[5]] ||  data[5]
-    if constituency.blank?
-      # raise "No constituency specified for MP: #{data[0]}"
-      puts "No constituency specified for MP: #{data[0]}"
-    else
-      state = State.first(:name.like => data[4])
-      cs = state.constituencies.all(:name.like => constituency) + 
-           state.constituencies(:name.like => "#{constituency}(SC)") + Constituency.all(:name.like => "#{constituency} (SC)") + 
-           state.constituencies(:name.like => "#{constituency}(ST)") + Constituency.all(:name.like => "#{constituency} (ST)")
-      # raise "Incorrect no. of constituencies '#{data[5]}' - #{cs.size}" if cs.size !=1
-      puts "Incorrect no. of constituencies '#{data[5]}' - #{cs.size}" if cs.size !=1
-      return cs.first
-    end    
+    constituency = CONSTITUENCY_ALIASES[data[5]] ||  data[5] || "Nominated"
+    state = State.first(:name.like => data[4])
+    cs = state.constituencies.all(:name.like => constituency) + 
+    state.constituencies(:name.like => "#{constituency}(SC)") + Constituency.all(:name.like => "#{constituency} (SC)") + 
+    state.constituencies(:name.like => "#{constituency}(ST)") + Constituency.all(:name.like => "#{constituency} (ST)")
+    raise "Incorrect no. of constituencies '#{data[5]}' - #{cs.size}" if cs.size !=1
+    return cs.first
   end
   
   def create_statistic constituency, data
     constituency.mp.update(:mp_statistic=> {:debates => data[11].to_i, :private_member_bills => data[12].to_i, 
                            :questions => data[13].to_i, :attendance => data[14].delete("%").to_i})
                            
-    valid = constituency.state.update(:state_statistic => {:debates => data[20].to_f, :private_member_bills => data[21].to_f,
+    constituency.state.update(:state_statistic => {:debates => data[20].to_f, :private_member_bills => data[21].to_f,
                                :questions => data[22].to_f, :attendance => data[23].delete("%").to_f})
-    raise "#{constituency.state.state_statistic}" unless valid
   end
   
   def verify
