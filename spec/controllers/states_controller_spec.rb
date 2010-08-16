5b5a23411104386d79267223ec97a9357dc1daff
Akshay Rawat <akshay@activesphere.com>
2010-08-16 17:39:27 +0530

    api now exposes state statistics

--- spec/controllers/states_controller_spec.rb
+++ spec/controllers/states_controller_spec.rb
@@ -4,7 +4,7 @@ describe StatesController do
   
   context "index" do
     it "should list all states when format is json" do
-      state_one = Factory.create(:state)
+      state_one = Factory.create(:state, :state_statistic => StateStatistic.create(:attendance => 90.5))
       state_two = Factory.create(:state)
       
       get :index, :format => :json
@@ -12,11 +12,12 @@ describe StatesController do
       states = ActiveSupport::JSON.decode(response.body)
       states.should have(2).things
       states.first["name"].should == state_one.name
+      states.first["state_statistic"]["attendance"].should == 90.5
       states.last["name"].should == state_two.name
     end
 
     it "should list all states when format is xml" do
-      state_one = Factory.create(:state)
+      state_one = Factory.create(:state, :state_statistic => StateStatistic.create(:attendance => 90.5))
       state_two = Factory.create(:state)
       
       get :index, :format => :xml
@@ -24,6 +25,7 @@ describe StatesController do
       states =  Hash.from_xml(response.body)["states"]
       states.should have(2).things
       states.first["name"].should == state_one.name
+      states.first["state_statistic"]["attendance"].should == 90.5
       states.last["name"].should == state_two.name
     end
   end
