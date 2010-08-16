5be83dbba3e9ad2a95a504c17269a06b6e3893f9
Akshay Rawat <akshay@activesphere.com>
2010-08-16 18:12:46 +0530

    disabled state statistics due to problems

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -4,8 +4,8 @@ class StatesController < ApplicationController
   def index
     states = ::State.all
     respond_to do |format|
-      format.json {render :json => states.to_json
-      format.xml {render :json => states.to_xml
+      format.json {render :json => states.to_json}
+      format.xml {render :json => states.to_xml}
     end
   end
 
